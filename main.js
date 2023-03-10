ef812bce6659b4f899b294c102ce9178cb66c25e
Lisagraph <127458384+Lisagraph@users.noreply.github.com>
2023-03-11 06:53:38 +1100

    Delete main.js

--- main.js
+++
@@ -1,4 +0,0 @@
-const element = document.getElementById("scrollDIV");
-let y = element.scrollHeight;
-let x = element.scrollWidth;
-document.getElementById ("content").innerHTML = "Height: " + y + " Width: " + x;
