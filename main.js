817f0148314e8eebde7f39f5a25fcfd7b95edd35
Lisagraph <127458384+Lisagraph@users.noreply.github.com>
2023-03-11 06:50:11 +1100

    Add files via upload

---
+++ main.js
@@ -0,0 +1,4 @@
+const element = document.getElementById("scrollDIV");
+let y = element.scrollHeight;
+let x = element.scrollWidth;
+document.getElementById ("content").innerHTML = "Height: " + y + " Width: " + x;
